7586c2bd32140a0b719c6f951fe1313a9db57d58
rubledevloper <mrrubelislam4@gmail.com>
2023-10-06 19:39:40 +0600

    first commit

---
+++ project/script.js
@@ -0,0 +1,3 @@
+function toggle(){
+    document.getElementById('header-menu').classList.toggle('active');
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+// function showText(){
+//     document.getElementById('nayem').style.visibility = 'visible';
+// }
+// function hideText(){
+//     document.getElementById('nayem').style.visibility = 'hidden';
+// }
+
+function toggle(){
+    document.getElementById('nayem').classList.toggle('demo');
+}
